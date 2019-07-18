f749c3e64f39536fa65c90ffabbcd8aefe05ff98
yann.danot.c <yann.danot.c@thalesdigital.io>
2019-07-18 12:09:25 +0200

    pair

--- src/main/kotlin/yams/Roll.kt
+++ src/main/kotlin/yams/Roll.kt
@@ -1,6 +1,6 @@
 package yams
 
-class Roll(val dices: List<Int>) {
+class Roll(private val dices: List<Int>) {
 
     fun chance(): Int = dices.sum()
     fun ones(): Int = dices.filter { it == 1 }.sum()
@@ -9,4 +9,17 @@ class Roll(val dices: List<Int>) {
     fun fours(): Int = dices.filter { it == 4 }.sum()
     fun fives(): Int = dices.filter { it == 5 }.sum()
     fun sixes(): Int = dices.filter { it == 6 }.sum()
+
+    fun pair(): Int {
+        var save: Int = 0
+
+        dices.sorted().reversed().forEach({
+            if (it == save) {
+                return it * 2
+            }
+            save = it
+        })
+
+        return 0
+    }
 }
---
+++ src/test/kotlin/yams/PairTest.kt
@@ -0,0 +1,18 @@
+package yams
+
+import org.junit.Test
+
+class PairTest {
+
+    @Test
+    internal fun should_sum_two_highest_matching_dices() {
+        pair_for("⚀ ⚁ ⚂ ⚃ ⚄") should_be_valuated 0
+        pair_for("⚀ ⚁ ⚂ ⚄ ⚄") should_be_valuated 10
+        pair_for("⚀ ⚁ ⚂ ⚂ ⚄") should_be_valuated 6
+        pair_for("⚀ ⚄ ⚂ ⚂ ⚄") should_be_valuated 10
+    }
+
+    private fun pair_for(s: String): Int {
+        return roll(s).pair()
+    }
+}
